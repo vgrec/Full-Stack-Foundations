ab920ee4854e41d274b3f447185e50c0789718fc
vgrec <grec.vs@gmail.com>
2018-12-15 22:00:56 +0100

    Add placeholder methods for new/edit/delete restaurant

--- framework/project.py
+++ framework/project.py
@@ -1,12 +1,44 @@
 from flask import Flask
+from flask import render_template
+from restaurants_repository import RestaurantsRepository
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/hello')
-def hello_world():
-    return "Hello World"
+@app.route('/restaurants/<int:restaurant_id>/')
+def home(restaurant_id):
+    repository = RestaurantsRepository()
+    items = repository.get_menu(restaurant_id)
+    output = ""
+    for item in items:
+        output += item.name
+        output += "<br/>"
+        output += item.price
+        output += "<br/>"
+        output += item.description
+        output += "<br/> <br/>"
+    return output
+
+
+@app.route('/restaurant/<int:restaurant_id>/new/')
+def new_menu_item(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
+def edit_menu_item(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+def delete_menu_item(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
+
+@app.route('/template/')
+def template():
+    return render_template("menu.html", item="Pasta")
 
 
 if __name__ == '__main__':
---
+++ framework/restaurants_repository.py
@@ -0,0 +1,39 @@
+from database_setup import Restaurant, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from restaurants.framework.database_setup import MenuItem
+
+
+class RestaurantsRepository:
+    def __init__(self):
+        engine = create_engine('sqlite:///restaurantmenu.db')
+        Base.metadata.bind = engine
+
+        DBSession = sessionmaker(bind=engine)
+        self.session = DBSession()
+
+    def get_restaurants(self):
+        return self.session.query(Restaurant).all()
+
+    def new_restaurant(self, restaurant_name):
+        self.session.add(Restaurant(name=restaurant_name))
+        self.session.commit()
+
+    def get_restaurant_by_id(self, id):
+        return self.session.query(Restaurant).filter_by(id=id).first()
+
+    def update_restaurant(self, id, name):
+        restaurant = self.get_restaurant_by_id(id)
+        restaurant.name = name
+        self.session.commit()
+
+    def get_menu(self, restaurant_id):
+        return self.session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+
+
+r = RestaurantsRepository()
+
+items = r.get_menu(1)
+for item in items:
+    print(item.name)
